41b9f5f600b6d1151f838a41eee7d2056642eb54
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2023-07-26 09:46:33 +0900

    世界はそれをブラクラと呼ぶんだぜ

--- hooks/useWatchClosestStation.ts
+++ hooks/useWatchClosestStation.ts
@@ -1,23 +1,31 @@
-import { useSetAtom } from "jotai";
-import { useEffect } from "react";
+import { useAtomValue, useSetAtom } from "jotai";
+import { useCallback, useEffect, useRef } from "react";
 import { navigationAtom } from "../atoms/navigation";
+import { stationAtom } from "../atoms/station";
 import useCurrentPosition from "./useCurrentPosition";
 
 const useWatchClosestStation = () => {
+  const { selectedBound } = useAtomValue(stationAtom);
   const setNavigationAtom = useSetAtom(navigationAtom);
+  const watchingIdRef = useRef<number | null>(null);
 
-  const handlePositionUpdate = (location: GeolocationPosition) =>
-    setNavigationAtom((prev) => ({ ...prev, location }));
+  const handlePositionUpdate = useCallback(
+    (location: GeolocationPosition) =>
+      setNavigationAtom((prev) => ({ ...prev, location })),
+    [setNavigationAtom]
+  );
 
   const { watchPosition } = useCurrentPosition({
     onPositionUpdate: handlePositionUpdate,
   });
 
   useEffect(() => {
-    const watchingId = watchPosition();
+    if (!watchingIdRef.current && selectedBound) {
+      watchingIdRef.current = watchPosition();
+    }
 
-    return () => navigator.geolocation.clearWatch(watchingId);
-  }, [watchPosition]);
+    return () => navigator.geolocation.clearWatch(watchingIdRef.current ?? 1);
+  }, [selectedBound, watchPosition]);
 };
 
 export default useWatchClosestStation;
