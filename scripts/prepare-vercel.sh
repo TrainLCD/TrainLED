e80b6d5e037fa4d5db350d30eb4ab60ae41bd737
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2023-12-12 06:04:38 +0000

    yarn

--- scripts/prepare-vercel.sh
+++ scripts/prepare-vercel.sh
@@ -3,4 +3,4 @@ yum install -y wget
 wget https://github.com/protocolbuffers/protobuf/releases/download/v25.1/protoc-25.1-linux-x86_64.zip
 unzip protoc-25.1-linux-x86_64.zip -d protoc
 export PATH=$PATH:./protoc/bin
-npm install
+yarn install
