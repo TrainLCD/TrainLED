8d8b34b688caedf22edef48eb2646478934ebcbb
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2024-02-27 11:53:32 +0900

    changes for ios support

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import type { Viewport } from "next";
 import { Metadata } from "next";
 import localFont from "next/font/local";
 import StyledComponentsRegistry from "../lib/registry";
@@ -7,6 +8,7 @@ import Provider from "./provider";
 export const metadata: Metadata = {
   title: "TrainLED",
   description: "A joking navigation app.",
+  appleWebApp: true,
 };
 
 const jfDotFont = localFont({
@@ -14,6 +16,11 @@ const jfDotFont = localFont({
   display: "swap",
   variable: "--font-jfdot",
 });
+
+export const viewport: Viewport = {
+  themeColor: "#212121",
+  viewportFit: "cover",
+};
 
 export default function RootLayout({
   children,
