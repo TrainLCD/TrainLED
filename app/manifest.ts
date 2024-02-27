9a58a6b036c0a1f2da807b6862a15fcc5e5a9b7a
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2024-02-27 15:19:20 +0900

    add manifest.ts

---
+++ app/manifest.ts
@@ -0,0 +1,20 @@
+import { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "TrainLED",
+    short_name: "TrainLED",
+    description: "A joking navigation app.",
+    start_url: "/",
+    theme_color: "#ffffff",
+    background_color: "#212121",
+    display: "standalone",
+    icons: [
+      {
+        src: "/icon.png",
+        sizes: "any",
+        type: "image/png",
+      },
+    ],
+  };
+}
