b679b7ea9f551eaf2aef0d47c15b9739ee17e52d
Tsubasa SEKIGUCHI <oss@tinykitten.me>
2025-02-01 18:31:10 +0900

    DOレイアウト修正

--- components/DevOverlay.styled.ts
+++ components/DevOverlay.styled.ts
@@ -6,11 +6,12 @@ export const Container = styled.div`
   width:256px;
   top: 0;
   right:0;
-  background-color: black
+  background-color: black;
+  padding: 8px;
 `;
 
 export const Typography = styled.p`
-  line-height:0.5;
+  line-height: 1.25;
   font-size: 14px;
-  padding: 0 14px
+  margin: 0;
 `;
