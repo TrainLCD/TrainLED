fa342532529304608920deaa80aed0a0ff1195bc
Tsubasa SEKIGUCHI <oss@tinykitten.me>
2025-02-01 18:58:39 +0900

    カナリア版の時だけフッタに注意書きを出す

--- components/CommonFooter.tsx
+++ components/CommonFooter.tsx
@@ -4,13 +4,19 @@ import {
   FeedbackLink,
 } from "./CommonFooter.styled";
 
-const CommonFooter = () => (
-  <CreditContainer>
-    <CautionText>※このTrainLEDはカナリア版です。</CautionText>
-    <FeedbackLink href="https://forms.gle/a887VmUTWppB7v5Z8">
-      カナリア版のフィードバックはこちら
-    </FeedbackLink>
-  </CreditContainer>
-);
+const CommonFooter = () => {
+  if (!process.env.CANARY) {
+    return null;
+  }
+
+  return (
+    <CreditContainer>
+      <CautionText>※このTrainLEDはカナリア版です。</CautionText>
+      <FeedbackLink href="https://forms.gle/a887VmUTWppB7v5Z8">
+        カナリア版のフィードバックはこちら
+      </FeedbackLink>
+    </CreditContainer>
+  );
+};
 
 export default CommonFooter;
